72dd67a0f7c7f57e0023121fb9c9ad1121f66bd9
andrew <andrewscaldwell@gmail.com>
2025-07-30 20:37:46 -0400

    more asset resolution on gh pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  
-  ssr: false,
   app: {
-    baseUrl: '/which-alejandro/',
+    baseURL: '/which-alejandro/',
+    buildAssetsDir: '_nuxt/'    
+  },
+  nitro: {
+    preset: 'static'
+  },
+  routeRules: {
+    // optional but helps ensure 404s redirect to index.html
+    '/**': { static: true }
   }
 })
